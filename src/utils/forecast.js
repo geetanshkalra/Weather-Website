697c1441c53b4b81d447824308417fc1122cfe94
Geetansh kalra <geetanshkalra30@gmail.com>
2021-04-29 11:25:35 +0530

    Changed Some things

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,14 @@
 const request = require('request')
 
 const forecast = (lantitude, longitude, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=99fad31178f4f6e459d6d6df0dbfc4db&query=' + longitude + ',' + lantitude + '&units=f'
-
+    const url = 'http://api.weatherstack.com/current?access_key=99fad31178f4f6e459d6d6df0dbfc4db&query=' + lantitude + ',' + longitude 
     request({ url, json: true }, (error, {body}) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined)
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " degress out.")
+            callback(undefined,"It is currently " + body.current.temperature + " C degress out," + "But it feels like " + body.current.feelslike + " C "   + " It's " +  body.current.weather_descriptions[0] + " Out." + " The humidity level is " +body.current.humidity )
         }
     })
 }
